60587871ce859509a8bc2dd10790548623d7610d
Lynsey Powell <lynseympowell@gmail.com>
2019-02-09 14:19:31 -0700

    change mobile header

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+
+function setHeader() {
+ if ($(window).width() < 768) {
+   $('.mobile-header').addClass('active-header');
+ } else {
+   $('.full-header').addClass('active-header');
+ }
+}
+
+
 function handleMenubutton() {
   $('#menu').click(event => {
     event.preventDefault();
-    $('nav').toggleClass('hidden');
+    $('nav').toggleClass('open');
   })
 }
 
 function handleStartPage() {
+  setHeader();
   handleMenubutton();
   }
 
