39edf1e651800a47c7a4269b4be9c4dd7dfef3d1
Lynsey Powell <lynseympowell@gmail.com>
2019-02-10 14:47:52 -0700

    Ipad responsive detailing

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 
 function setHeader() {
- if ($(window).width() < 768) {
-   $('.mobile-header').addClass('active-header');
- } else {
-   $('.full-header').addClass('active-header');
- }
+  if ($(window).width() < 769) {
+    $('.mobile-header').addClass('active-header');
+  } else {
+    $('.full-header').addClass('active-header');
+  }
 }
-
 
 function handleMenubutton() {
   $('#menu').click(event => {
     event.preventDefault();
     $('nav').toggleClass('open');
-    $('.content-veil').toggleClass('open');
   })
 }
 
 function handleStartPage() {
   setHeader();
   handleMenubutton();
-  }
+}
 
-  $(handleStartPage);
+$(handleStartPage);
