813025d293a48a68f3f8b4bdc5c9796c56c4000d
Lynsey Powell <lynseympowell@gmail.com>
2019-02-10 19:35:02 -0700

    Fix aXe violations

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
-//set header based on screen width
+//Set header based on screen width
 function setHeader() {
   if ($(window).width() < 769) {
+    $('header').addClass('mobile-header');
     $('.mobile-header').addClass('active-header');
   } else {
+    $('header').addClass('full-header');
     $('.full-header').addClass('active-header');
   }
 }
 
-//open menu options for smaller screens
+//Open menu options on mobile
 function handleMenubutton() {
   $('#menu').click(event => {
     event.preventDefault();
